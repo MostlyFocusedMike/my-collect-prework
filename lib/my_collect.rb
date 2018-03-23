03be065a91fca645a59744f33e0738a7cc759878
Mike C <mikecronin92@gmail.com>
2018-03-23 20:11:07 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+      i = 0
+      new_arr = []
+      while i < array.length
+        new_val = yield(array[i])
+        new_arr << new_val
+        i = i + 1
+      end
 
+      return new_arr
+    end
 
