a89f2122bddc7e94fce77aa6f6808cc66bb3764b
Mike C <mikecronin92@gmail.com>
2018-03-23 20:12:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
-def hello_t(collection)
-      i = 0
-      new_arr = []
-      while i < collection.length
-        new_val = yield(collection[i])
-        new_arr << new_val
-        i = i + 1
-      end
+def my_collect(collection)
+  i = 0
+  new_arr = []
+  while i < collection.length
+    new_val = yield(collection[i])
+    new_arr << new_val
+    i = i + 1
+  end
 
-      return new_arr
-    end
+  return new_arr
+end
 
